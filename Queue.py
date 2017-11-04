24cd60a991b026bd9c98957dc2cdcba5a9cf3c13
PRIYADHARSHINI GUNARAJ <pgunaraj@asu.edu>
2017-11-04 10:26:26 -0700

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -1,5 +1,5 @@
 # QUEUE
-
+#using list
 class Queue:
     def __init__(self):
         self.items = []
@@ -32,3 +32,47 @@ q.dequeue()
 q.dequeue()
 print(q.size())
 q.printQueue()
+
+#using LL
+class Node:
+	def __init__(self,v):
+		self.data=v
+		self.next=None
+
+class Queue:
+	def __init__(self):
+		self.front=None
+		self.rear=None
+	def enqueue(self,v):
+		temp=Node(v)
+		if not self.front:
+			self.front=temp
+			self.rear=temp
+		else:
+			self.rear.next=temp
+			self.rear=temp
+	def dequeue(self):
+		if not self.front:
+			return
+		else:
+			temp=self.front
+			self.front=self.front.next
+		if not self.front:
+			self.rear=None
+		return temp
+	def printQueue(self):
+		temp=self.front
+		while temp:
+			print(temp.data,end=" ")
+			temp=temp.next
+		print()
+	def top(self):
+		return self.front.data
+
+q=Queue()
+q.enqueue(2)
+q.enqueue(3)
+q.enqueue(1)
+q.printQueue()
+q.dequeue()
+print(q.top())
