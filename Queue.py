081cc89a2d204b2d95f925e885295ad3fea0a988
Priyadharshini Gunaraj <pgunaraj@asu.edu>
2019-03-14 20:59:41 -0600

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -1,4 +1,112 @@
 # QUEUE
+#using Array with extra size parameter
+class Queue:
+	def __init__(self,size):
+		self.size=size
+		self.front=self.len=0
+		self.rear=-1
+		self.queue=[None]*size
+	def enqueue(self,v):
+		if (self.len==self.size):
+			print("Q FULL, can't insert")
+		else:
+			self.rear=(self.rear+1)%self.size
+			self.queue[self.rear]=v
+			self.len+=1
+	def dequeue(self):
+		if self.len==0:
+			print("Q EMPTY,can't delete")
+		else:
+			self.front=(self.front+1)%self.size
+			self.len-=1
+	def display(self):
+		print("len",self.len)
+		if self.len==0:
+			print("Q EMPTY")
+		elif self.rear>self.front:
+			for i in range(self.front,self.rear+1):
+				print(self.queue[i],end=" ")
+			print()
+		else:
+			for i in range(self.front,self.size):
+				print(self.queue[i],end=" ")
+			for i in range(self.rear+1):
+				print(self.queue[i],end=" ")
+			print()
+
+q=Queue(3)
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.enqueue(4)
+q.display()
+q.dequeue()
+q.dequeue()
+q.enqueue(5)
+q.display()
+q.dequeue()
+q.dequeue()
+q.dequeue()
+
+
+#using Array without extra size parameter
+class Queue:
+	def __init__(self,size):
+		self.size=size
+		self.front=0
+		self.rear=-1
+		self.queue=[None]*size
+	def enqueue(self,v):
+		if (((self.rear+1)%self.size==self.front) and self.rear!=-1):
+			print("Q FULL, can't insert")
+		else:
+			self.rear=(self.rear+1)%self.size
+			self.queue[self.rear]=v
+	def dequeue(self):
+		if self.rear==-1:
+			print("Q EMPTY,can't delete")
+		elif self.front==self.rear:
+			self.front=0
+			self.rear=-1
+		else:
+			self.front=(self.front+1)%self.size
+	def len(self):
+		if self.rear==-1:
+			print("Q EMPTY")
+		elif self.rear>self.front:
+			print("len",self.rear-self.front+1)
+		else:
+			print("len",(self.size-self.front)+self.rear+1)
+	def display(self):
+		if self.rear==-1:
+			print("Q EMPTY")
+		elif self.rear>self.front:
+			for i in range(self.front,self.rear+1):
+				print(self.queue[i],end=" ")
+			print()
+		else:
+			for i in range(self.front,self.size):
+				print(self.queue[i],end=" ")
+			for i in range(self.rear+1):
+				print(self.queue[i],end=" ")
+			print()
+
+q=Queue(3)
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.enqueue(4)
+q.len()
+q.display()
+q.dequeue()
+q.dequeue()
+q.enqueue(5)
+q.len()
+q.display()
+q.dequeue()
+q.dequeue()
+q.dequeue()
+
 #using list
 class Queue:
     def __init__(self):
