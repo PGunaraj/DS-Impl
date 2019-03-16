08f6514246b353acdaa26aee7ab94ea87b8f4176
Priyadharshini Gunaraj <pgunaraj@asu.edu>
2019-03-15 21:48:29 -0600

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -1,4 +1,76 @@
 class Node:
+	def __init__(self,val):
+		self.data=val
+		self.next=None
+		
+class LL:
+	def __init__(self):
+		self.head=None
+	
+	def add(self,val):
+		new=Node(val)
+		if self.head is None:
+			self.head=new
+		else:
+			node=self.head
+			while node.next != None:
+				node=node.next
+			node.next=new
+	
+	def delete(self,val):
+		if self.head is None:
+			print("LL EMPTY, can't delete")
+		elif self.head.data==val:
+			self.head=self.head.next
+		else:
+			prev=self.head
+			node=prev.next
+			while node is not None and node.data!=val:
+				node=node.next
+			if node is None:
+				print("Error!Not Found")
+			else:
+				prev.next=node.next
+	
+	def display(self):
+		if self.head is None:
+			print("LL EMPTY")
+		node=self.head
+		while node != None:
+			print(node.data,end=" ")
+			node=node.next
+	
+	def size(self):
+		l=0
+		if self.head is None:
+			print(l)
+		else:
+			l+=1
+			node=self.head
+			while node.next != None:
+				l+=1
+				node=node.next
+			print(l)
+
+l=LL()
+l.add(1)
+l.add(2)
+l.add(3)
+l.display()
+l.size()
+l.delete(2)
+l.display()
+l.delete(5)
+l.delete(3)
+l.display()
+l.size()
+l.delete(1)
+l.display()
+l.size()
+
+
+
+class Node:
     def __init__(self, inidata):
         self.data = inidata
         self.next = None
